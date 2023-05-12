cf9a5eea27e1ccc0f23cba43ca30df7a0b155aca
Piotr Jarnecki <piotr.jarnecki@gmail.com>
2023-05-12 11:21:44 +0200

    CarsListApplication

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -0,0 +1,35 @@
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.BMW;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Saab;
+
+import java.util.ArrayList;
+public class CarsListApplication {
+    public static void main(String[] args) {
+
+        ArrayList<Car> carArrayList = new ArrayList<>();
+
+        carArrayList.add( new BMW());
+        Saab saab = new Saab();
+        carArrayList.add(saab);
+        carArrayList.add(new Ford());
+
+        System.out.println("Wstępna lista aut:");
+        for(Car car : carArrayList){
+            CarUtils.describeCar(car);
+        }
+        carArrayList.remove(0);
+
+        carArrayList.remove(saab);
+
+        System.out.println("Końcowa lista aut:");
+        for (Car car : carArrayList){
+            CarUtils.describeCar(car);
+        }
+
+
+    }
+}
